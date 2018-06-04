fee3fb760974ff205a32661431df66208fc78abd
craigerl <craig@craiger.org>
2018-06-04 11:20:59 -0700

    send position every 10 mins so we show up on map

--- ygate.py
+++ ygate.py
@@ -5,27 +5,40 @@ import telnetlib
 import time
 import threading
 
-
+# Please fill these out accordingly
 HOST = "noam.aprs2.net"     # north america tier2 servers round robin
 USER = "KM6XXX-1"
 PASS = "00000"
+POSITION = "3899.70NR12099.15W"
 
+
+def send_my_position():     # tell aprsis our name and position every 10 mins (1200secs)
+  threading.Timer(1200, send_my_position).start()
+  position_string = USER + ">APRS,TCPIP*:!" + POSITION + "&Yaesu Ygate https://github.com/craigerl/ygate \n"
+  tn.write(position_string)
+  print position_string.strip()
+
+
+#connect to aprs server using telnet
 try:
   tn = telnetlib.Telnet(HOST, 14580)
 except Exception, e:
   print "Telnet session failed.\n"
   sys.exit(-1)
 time.sleep(2)
+
 #login
 tn.write("user " + USER + " pass " +  PASS + " vers ygate.py 0.99\n" )
-#send position, please uncomment, then change your position and call
-#tn.write("KM6XXX-1>SXUTWP,WIDE1-1:!3899.70NR12099.15W&Yaesu Ygate https://github.com/craigerl/ygate \n")
 
+#read a line from the aprsis server
 tnline=""
 for char in tn.read_until("\n",100):
   tnline = tnline + char
 tnline = tnline.replace('\n', '')
 print tnline
+
+#start position send thread
+send_my_position()
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
@@ -35,7 +48,13 @@ while True:
      routing = line.strip()
      payload = ser.readline().strip() 
      packet = routing + payload
+     if len(payload) == 0:
+       print ">>> No payload, not gated:  " + packet
+       break 
      packet = re.sub(' \[.*\] <UI.*>:', ':', packet)
+     if ',TCP' in routing:
+       print ">>> Packet from internet not gated: " + packet + "\n"
+       break 
      print  packet
      tn.write(packet + '\n')
 
