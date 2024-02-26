cfd7a71df15b504884710ab6e6569516c84e245a
Craig Lamparter <craig@craiger.org>
2024-02-26 13:58:15 -0800

    header update

--- ygate.py
+++ ygate.py
@@ -9,8 +9,8 @@
 #  a receive-only APRS igate.  All APRS packet traffic your radio hears will be
 #  forwarded to the Internet (APRS-IS Servers) for further routing.
 #
-#  Hook your Yaesu amateur radio with APRS up to a Linux pc (raspberry pi) with a
-#  USB cable. Set the radio to emit "packet" data in nmea9 format over the data
+#  Hook your Yaesu amateur radio with APRS up to a Linux pc (raspberry pi) with the
+#  Yaesu USB cable. Set the radio to emit "packet" data in nmea9 format over the data
 #  port. This script will login to an aprsis server, and relay the packet data
 #  after reformatting the reduculous output from the radio into real APRS packet
 #  strings.
